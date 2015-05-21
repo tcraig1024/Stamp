cc55a1aa376c087a450ea93e82a9d01f1fad064c
tcraig1024 <tcraig.1024@yahoo.com>
2015-05-21 16:47:42 -0500

    Update snowshoe.js

--- snowshoe/lib/snowshoe.js
+++ snowshoe/lib/snowshoe.js
@@ -1,12 +1,11 @@
-function client (3254d43c9096961b670a, 108c35d3355773f411a96d62a7dd116ea9ed13eb){
-  var data = {data: request.body.data}
+function client (appKey, appSecret){
   var OAuth = require('OAuth');
 
   this.oauth = new OAuth.OAuth(
     'http://beta.snowshoestamp.com/oauth/request_token',
     'http://beta.snowshoestamp.com/oauth/access_token',
-    3254d43c9096961b670a,
-    108c35d3355773f411a96d62a7dd116ea9ed13eb,
+    appKey,
+    appSecret,
     '1.0',
     null,
     'HMAC-SHA1'
@@ -15,13 +14,12 @@ function client (3254d43c9096961b670a, 108c35d3355773f411a96d62a7dd116ea9ed13eb)
   this.post = function(points, response, callback){
     this.oauth.post(
       'http://beta.snowshoestamp.com/api/v2/stamp',
-      'http://tcraig1024.github.io/Stamp/Success.html',
-      'http://tcraig1024.github.io/Stamp/Logs/Success.log',
+      '',
+      '',
       points,
       function (error, data, oauth_response){
         if (error) {
-          error = JSON.parse(error);
-          callback(error, null, response);
+          return callback(error, null, response);
         };
         data = JSON.parse(data);
         callback(null, data, response);
