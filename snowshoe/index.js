8f9e54309a5d63936cabb9dab1f8964af2080e4d
tcraig1024 <tcraig.1024@yahoo.com>
2015-05-21 16:55:36 -0500

    Update index.js

--- snowshoe/index.js
+++ snowshoe/index.js
@@ -1,2 +1,12 @@
 var snowshoe = require('./lib/snowshoe');
+var client = new Snowshoe.client(SNOWSHOE_APP_KEY, SNOWSHOE_APP_SECRET)
+var data = {data: request.body.data}
+// in order for the callback to behave correctly inside OAuth 
+// you must include the route's response object as the second argument 
+client.post(data, response, function(error, data, response)
+{ if (error) { 
+// handle errors 
+}; 
+// handle success 
+})
 module.exports = snowshoe;
